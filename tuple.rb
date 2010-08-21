1a8752d8f7c04ace133a31784887c1f3146f5ae7
Ryan Davis <ryand@zenspider.com>
2010-08-20 18:49:47 -0800

    Minor style fixes [git-p4: depot-paths = "//src/RubyAudit/dev/": change = 5809]

--- tuple.rb
+++ tuple.rb
@@ -3,10 +3,10 @@
 module Enumerable
   def deepfreeze
     self.each do |o|
-      if o != self then 
-	o.deepfreeze
+      if o != self then
+        o.deepfreeze
       else
-	o.freeze
+        o.freeze
       end
     end
     self.freeze
